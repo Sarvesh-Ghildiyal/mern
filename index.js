2e25f793851dbef7941ee3e39d0f2993d7352ed7
Sarvesh-Ghildiyal <sarveshghildiyal@gmail.com>
2023-07-01 13:46:02 +0530

    done with js display methods

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+
+document.write('helo')
 document.getElementById('first').innerHTML='he~ he~ he~';
+
 console.log('My first code in js');
 
 var a=5; 
@@ -6,6 +9,13 @@ const b=6
 let c=9
 console.log(a,b,c)
 
-for(let i=0;i<10;i++){
+let sum=0
+for(let i=1;i<=10;i++){
     console.log(i)
+    sum=i+sum;
+}
+console.log(sum)
+
+function al(){
+    alert('he he he hehe');
 }
