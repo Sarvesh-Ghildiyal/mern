520edf75fc94762be88343bc3aac67335710b645
Sarvesh-Ghildiyal <sarveshghildiyal@gmail.com>
2023-07-01 10:52:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('first').innerHTML='he~ he~ he~';
+console.log('My first code in js');
+
+var a=5; 
+const b=6
+let c=9
+console.log(a,b,c)
+
+for(let i=0;i<10;i++){
+    console.log(i)
+}
